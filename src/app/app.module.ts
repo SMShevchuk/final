7611df2537ead5b06f79161d53348f58fb4e5a1d
SergiiShevchuk <smshevchuk1987@gmail.com>
2021-11-21 19:35:24 +0200

    post2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Post4Component } from './post4/post4.component';
+import { Post2Component } from './post2/post2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Post4Component
+    Post4Component,
+    Post2Component
   ],
   imports: [
     BrowserModule,
