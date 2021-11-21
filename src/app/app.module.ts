669641a5d7a8743f6d275e5494dbac93c6fc9e39
Vitiok2021 <viktorpts1vol@gmail.com>
2021-11-21 19:24:17 +0200

    e54

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Post4Component } from './post4/post4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Post4Component
   ],
   imports: [
     BrowserModule,
