b6a4ff1dc2657d5ac64264020270a2039fffb0eb
SergiiShevchuk <smshevchuk1987@gmail.com>
2021-11-21 19:49:40 +0200

    post 1 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Post4Component } from './post4/post4.component';
 import { Post2Component } from './post2/post2.component';
+import { Post1Component } from './post1/post1.component';
+import { Post3Component } from './post3/post3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Post4Component,
-    Post2Component
+    Post2Component,
+    Post1Component,
+    Post3Component
   ],
   imports: [
     BrowserModule,
