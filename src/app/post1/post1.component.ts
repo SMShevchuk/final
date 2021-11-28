309aef7924563938020af37c78c9ba765b171288
pVestt <pashavestt2010@gmail.com>
2021-11-28 15:57:45 +0300

    fg

--- src/app/post1/post1.component.ts
+++ src/app/post1/post1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import {
   trigger,
   state,
@@ -11,46 +11,87 @@ import {
   selector: 'app-post1',
   templateUrl: './post1.component.html',
   styleUrls: ['./post1.component.scss'],
+
   animations: [
     trigger('openClose', [
-      state('true', style({
-         //height: '*',
-         //display: 'none', 
-         //transform: 'translateX(0%) translateY(0%)',
-         //backgroundColor: 'red', 
-        })),
-      state('false', style({
-         //height: '*' ,
-         //transform: 'translateX(110%) translateY(110px)', 
-        
-        })),
-      //transition('false <=> true', animate(500)),
-     
       transition("void => *", [
-        style({
-          opacity: 0,
-          transform: 'translateX(100%) translateY(0%)',
-        }),
-        animate(800)
-       ]),
-   
-    ])
-        
-  ]
+          style({
+            opacity: 0,
+            
+          }),
+          animate('3s', style({opacity: 1}))
+      ]),
+    ]),
+    /*---- 1 ----*/
+    trigger('openClose1', [
+      transition("void => *", [
+          style({
+            opacity: 0,
+            transform: 'translateY(-150px) rotate(150deg)',
+          }),
+          animate('1s', style({
+            opacity: 1,
+            transform: 'translateY(0px) rotate(0deg)',
+          }))
+      ]),
+    ]),
+     /*---- 2 ----*/
+     trigger('openClose2', [
+      transition("void => *", [
+          style({
+            opacity: 0,
+            transform: 'translateX(150px) rotate(0deg) scale(2)',
+          }),
+          animate('1.2s', style({
+            opacity: 1,
+            transform: 'translateX(0px) rotate(0deg)',
+          }))
+      ]),
+    ]),
+      /*---- 3 ----*/
+      trigger('openClose3', [
+        transition("void => *", [
+            style({
+              opacity: 0,
+              transform: 'translateX(-150px) rotate(0deg)',
+            }),
+            animate('.9s', style({
+              opacity: 1,
+              transform: 'translateX(0px) rotate(0deg)',
+            }))
+        ]),
+      ]),
+      /*---- 4 ----*/
+      trigger('openClose4', [
+        transition("void => *", [
+            style({
+              opacity: 0,
+              transform: 'translateX(50px) rotate(40deg) scaleY(1.3)',
+            }),
+            animate('1.3s', style({
+              opacity: 1,
+              transform: 'translateX(0px) rotate(0deg)',
+            }))
+        ]),
+      ]),
+  ],
 })
 export class Post1Component implements OnInit {
 
- 
-
   open = true;
 
-
-
-
-
   constructor() { }
-
+ 
   ngOnInit(): void {
-  }
+    //this.open = false;
+    this.open = false;
 
+    setTimeout(()=>{ // <<<---using ()=> syntax
+    this.open = true;
+    
+    }, 500);
+
+
+    console.log('ngOnInit', this.open);
+  }
 }
