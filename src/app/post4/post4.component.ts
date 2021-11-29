331333f7ba1eb3b189b9170238ba16f60f1db8f4
Vitiok2021 <viktorpts1vol@gmail.com>
2021-11-29 19:18:40 +0200

    dsf

--- src/app/post4/post4.component.ts
+++ src/app/post4/post4.component.ts
@@ -1,15 +1,36 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post4',
   templateUrl: './post4.component.html',
-  styleUrls: ['./post4.component.scss']
+  styleUrls: ['./post4.component.scss'],
+  animations: [
+    trigger('openClose', [
+      transition('void=>*', [
+        style({
+          opacity: 0,
+          transform: 'translateX(100%)',
+        }),
+        animate('3s', style({
+          opacity: 1,
+          transform: 'translateX(0)',
+        }))
+      ]),
+    ]),
+  ]
 })
 export class Post4Component implements OnInit {
+
+  open = true;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.open = false;
+    setTimeout(() => {
+      this.open = true;
+    }, 500);
   }
 
 }
