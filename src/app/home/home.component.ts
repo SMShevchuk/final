8cd1e9c949c85b4adc0b37a2ff0bf3dd65f8d2f0
pVestt <pashavestt2010@gmail.com>
2021-11-29 22:48:18 +0300

    fg

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,70 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('openClose', [
+      transition("void => *", [
+          style({
+            opacity: 0,
+            
+          }),
+          animate('3s', style({opacity: 1}))
+      ]),
+    ]),
+    /*---- 1 ----*/
+    trigger('openClose1', [
+      transition("void => *", [
+          style({
+            opacity: 0,
+            transform: 'translateY(-150px) rotate(150deg)',
+          }),
+          animate('1s', style({
+            opacity: 1,
+            transform: 'translateY(0px) rotate(0deg)',
+          }))
+      ]),
+    ]),
+     /*---- 2 ----*/
+     trigger('openClose2', [
+      transition("void => *", [
+          style({
+            opacity: 0,
+            transform: 'translateX(150px) rotate(0deg) scale(2)',
+          }),
+          animate('1.2s', style({
+            opacity: 1,
+            transform: 'translateX(0px) rotate(0deg)',
+          }))
+      ]),
+    ]),
+     
+  ],
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  open = true;
 
+  constructor() { }
+ 
   ngOnInit(): void {
+    //this.open = false;
+    this.open = false;
+
+    setTimeout(()=>{ // <<<---using ()=> syntax
+    this.open = true;
+    
+    }, 500);
+    console.log('ngOnInit', this.open);
   }
 
 }
